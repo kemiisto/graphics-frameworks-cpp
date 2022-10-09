43f51095492ca1f95936f19b798c1137c14ea420
kemiisto <kemiisto@gmail.com>
2022-10-09 15:45:33 +0300

    feat: add Test_2_6

---
+++ src/02/test_2_6.cpp
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <tinygl/tinygl.h>
+#include <iostream>
+
+class Window final : public tinygl::Window
+{
+public:
+    using tinygl::Window::Window;
+    void init() override;
+    void draw() override;
+private:
+    tinygl::ShaderProgram program;
+    tinygl::Buffer vbo{tinygl::Buffer::Type::VertexBuffer, tinygl::Buffer::UsagePattern::StaticDraw};
+    tinygl::VertexArrayObject vao;
+    int translationLocation{-1};
+    int baseColorLocation{-1};
+};
+
+void Window::init()
+{
+    program.addShaderFromSourceFile(tinygl::Shader::Type::Vertex, "test_2_6.vert");
+    program.addShaderFromSourceFile(tinygl::Shader::Type::Fragment, "test_2_6.frag");
+    program.link();
+
+    vao.bind();
+
+    const GLfloat positionData[] = {
+         0.0f,  0.2f, 0.0f,
+         0.2f, -0.2f, 0.0f,
+        -0.2f, -0.2f, 0.0f
+    };
+    vbo.bind();
+    vbo.fill(positionData, sizeof(positionData));
+
+    auto attributeLocation = program.attributeLocation("position");
+    vao.setAttributeArray(attributeLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
+    vao.enableAttributeArray(attributeLocation);
+
+    translationLocation = program.uniformLocation("translation");
+    baseColorLocation = program.uniformLocation("baseColor");
+}
+
+void Window::draw() {
+    program.use();
+    vao.bind();
+
+    // draw the first triangle
+    program.setUniformValue(translationLocation, {-0.5f, 0.0f, 0.0f});
+    program.setUniformValue(baseColorLocation, {1.0f, 0.0f, 0.0f});
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    // draw the second triangle
+    program.setUniformValue(translationLocation, {0.5f, 0.0f, 0.0f});
+    program.setUniformValue(baseColorLocation, {0.0f, 0.0f, 1.0f});
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+MAIN
---
+++ src/02/test_2_6.vert
@@ -0,0 +1,9 @@
+#version 330 core
+
+in vec3 position;
+uniform vec3 translation;
+
+void main() {
+	vec3 pos = position + translation;
+	gl_Position = vec4(pos, 1.0);
+}
