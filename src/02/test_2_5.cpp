80263a09fd0d575f21105f309f2daa08b56e1dd2
kemiisto <kemiisto@gmail.com>
2022-10-09 13:55:14 +0300

    feat: add Test_2_5

---
+++ src/02/test_2_5.cpp
@@ -0,0 +1,66 @@
+#include "main.h"
+#include <tinygl/tinygl.h>
+#include <iostream>
+
+class Window final : public tinygl::Window
+{
+public:
+    using tinygl::Window::Window;
+    void init() override;
+    void draw() override;
+private:
+    tinygl::ShaderProgram program;
+    tinygl::Buffer vboPosition{tinygl::Buffer::Type::VertexBuffer, tinygl::Buffer::UsagePattern::StaticDraw};
+    tinygl::Buffer vboColor{tinygl::Buffer::Type::VertexBuffer, tinygl::Buffer::UsagePattern::StaticDraw};
+    tinygl::VertexArrayObject vao;
+};
+
+void Window::init()
+{
+    program.addShaderFromSourceFile(tinygl::Shader::Type::Vertex, "test_2_5.vert");
+    program.addShaderFromSourceFile(tinygl::Shader::Type::Fragment, "test_2_5.frag");
+    program.link();
+
+    glPointSize(10.0f);
+    glLineWidth(4.0f);
+
+    vao.bind();
+
+    const GLfloat positionData[] = {
+         0.8f,  0.0f, 0.0f,
+         0.4f,  0.6f, 0.0f,
+        -0.4f,  0.6f, 0.0f,
+        -0.8f,  0.0f, 0.0f,
+        -0.4f, -0.6f, 0.0f,
+         0.4f, -0.6f, 0.0f
+    };
+    vboPosition.bind();
+    vboPosition.fill(positionData, sizeof(positionData));
+
+    auto attributeLocation = program.attributeLocation("position");
+    vao.setAttributeArray(attributeLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
+    vao.enableAttributeArray(attributeLocation);
+
+    const GLfloat colorData[] = {
+        1.0f, 0.0f, 0.0f,
+        1.0f, 0.5f, 0.0f,
+        1.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f,
+        0.5f, 0.0f, 1.0f
+    };
+    vboColor.bind();
+    vboColor.fill(colorData, sizeof(colorData));
+
+    attributeLocation = program.attributeLocation("vertexColor");
+    vao.setAttributeArray(attributeLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
+    vao.enableAttributeArray(attributeLocation);
+}
+
+void Window::draw() {
+    program.use();
+    vao.bind();
+    glDrawArrays(GL_LINE_LOOP, 0, 6);
+}
+
+MAIN
---
+++ src/02/test_2_5.vert
@@ -0,0 +1,10 @@
+#version 330 core
+
+in vec3 position;
+in vec3 vertexColor;
+out vec3 color;
+
+void main() {
+    gl_Position = vec4(position, 1.0);
+    color = vertexColor;
+}
