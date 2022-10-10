7df7ef14d5cce764d6a08e3ee320e3a3e4d8dfbe
kemiisto <kemiisto@gmail.com>
2022-10-10 17:27:21 +0300

    feat: add Test_2_11

---
+++ src/02/test_2_11.cpp
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <tinygl/tinygl.h>
+#include <iostream>
+
+class Window final : public tinygl::Window
+{
+public:
+    using tinygl::Window::Window;
+    void init() override;
+    void processInput() override;
+    void draw() override;
+private:
+    tinygl::ShaderProgram program;
+    tinygl::Buffer vbo{tinygl::Buffer::Type::VertexBuffer, tinygl::Buffer::UsagePattern::StaticDraw};
+    tinygl::VertexArrayObject vao;
+    int translationLocation{-1};
+    int baseColorLocation{-1};
+    glm::vec3 translation{-0.5f, 0.0f, 0.0f};
+    GLfloat speed = 0.5f;
+};
+
+void Window::init()
+{
+    program.addShaderFromSourceFile(tinygl::Shader::Type::Vertex, "test_2_6.vert");
+    program.addShaderFromSourceFile(tinygl::Shader::Type::Fragment, "test_2_6.frag");
+    program.link();
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    vao.bind();
+
+    const GLfloat positionData[] = {
+         0.0f,  0.2f, 0.0f,
+         0.2f, -0.2f, 0.0f,
+        -0.2f, -0.2f, 0.0f
+    };
+    vbo.bind();
+    vbo.fill(positionData, sizeof(positionData));
+
+    auto attributeLocation = program.attributeLocation("position");
+    vao.setAttributeArray(attributeLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
+    vao.enableAttributeArray(attributeLocation);
+
+    translationLocation = program.uniformLocation("translation");
+    baseColorLocation = program.uniformLocation("baseColor");
+}
+
+void Window::processInput()
+{
+    auto distance = speed * deltaTime();
+    if (getKey(tinygl::Key::Left) == tinygl::KeyState::Press)
+        translation.x -= distance;
+    if (getKey(tinygl::Key::Right) == tinygl::KeyState::Press)
+        translation.x += distance;
+    if (getKey(tinygl::Key::Down) == tinygl::KeyState::Press)
+        translation.y -= distance;
+    if (getKey(tinygl::Key::Up) == tinygl::KeyState::Press)
+        translation.y += distance;
+}
+
+void Window::draw()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    program.use();
+    vao.bind();
+    program.setUniformValue(translationLocation, translation);
+    program.setUniformValue(baseColorLocation, {1.0f, 0.0f, 0.0f});
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+MAIN
